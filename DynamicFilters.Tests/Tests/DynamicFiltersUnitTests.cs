9cb3f17dcda839522e0edf636ca0ebb3c3ced0ff
Ask-DNA <alexander.sukhanov.11@gmail.com>
2024-10-17 22:33:58 +0300

    test: add test for equality filter option

---
+++ DynamicFilters.Tests/Tests/DynamicFiltersUnitTests.cs
@@ -0,0 +1,20 @@
+using DynamicFilters.Tests.DataLoaders;
+
+namespace DynamicFilters.Tests
+{
+    public class DynamicFiltersUnitTests
+    {
+        [Theory]
+        [ClassData(typeof(EqualityFiltrationBehaviorDataLoader))]
+        public void FiltrationBehavior_Equality_Success<T>(
+            List<T> subjectToFiltering,
+            DynamicFilterBase<T> filter,
+            Func<T, bool> analogousPredicate)
+        {
+            IEnumerable<T> result1 = subjectToFiltering.Where(filter);
+            IEnumerable<T> result2 = subjectToFiltering.Where(analogousPredicate);
+
+            Assert.Equal(result1, result2);
+        }
+    }
+}
