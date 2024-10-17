113b41ebd5b8f90677e49c44709176a8f866236f
Ask-DNA <alexander.sukhanov.11@gmail.com>
2024-10-17 22:58:46 +0300

    test: add test for multiple filter options usage

---
+++ DynamicFilters.Tests/DataLoaders/MultipleOptionFiltartionBehaviorDataLoader.cs
@@ -0,0 +1,51 @@
+﻿namespace DynamicFilters.Tests.DataLoaders
+{
+    public class MultipleOptionFiltartionBehaviorDataLoader : TheoryData<
+            List<MultipleOptionFiltartionBehaviorDataLoader.Entity>,
+            DynamicFilterBase<MultipleOptionFiltartionBehaviorDataLoader.Entity>,
+            Func<MultipleOptionFiltartionBehaviorDataLoader.Entity, bool>>
+    {
+        private static readonly List<Entity> _entities = [
+            new Entity() { IntegerField = 1, StringField = "1" },
+            new Entity() { IntegerField = 2, StringField = "2" },
+            new Entity() { IntegerField = 3, StringField = "3" },
+            new Entity() { IntegerField = 4, StringField = "4" },
+            new Entity() { IntegerField = 5, StringField = "5" },
+            new Entity() { IntegerField = 1, StringField = "5" },
+            new Entity() { IntegerField = 2, StringField = "4" },
+            new Entity() { IntegerField = 3, StringField = "3" },
+            new Entity() { IntegerField = 4, StringField = "2" },
+            new Entity() { IntegerField = 5, StringField = "1" }];
+
+        public MultipleOptionFiltartionBehaviorDataLoader()
+        {
+            Add(
+                _entities,
+                new Filter() { SomeInteger = 1, SomeString = "5" },
+                o => o.IntegerField == 1 && o.StringField != "5");
+            Add(
+                _entities,
+                new Filter() { SomeInteger = 3, SomeString = "3" },
+                o => o.IntegerField == 3 && o.StringField != "3");
+            Add(
+                _entities,
+                new Filter() { SomeInteger = 5, SomeString = "2" },
+                o => o.IntegerField == 5 && o.StringField != "2");
+        }
+
+        public class Entity
+        {
+            public int IntegerField = 0;
+            public string StringField = "";
+        }
+
+        public class Filter : DynamicFilterBase<Entity>
+        {
+            [FilterOption(FilterOptionType.Equality, nameof(Entity.IntegerField))]
+            public int SomeInteger = 0;
+
+            [FilterOption(FilterOptionType.Inequality, nameof(Entity.StringField))]
+            public string SomeString = "";
+        }
+    }
+}
--- DynamicFilters.Tests/Tests/DynamicFiltersUnitTests.cs
+++ DynamicFilters.Tests/Tests/DynamicFiltersUnitTests.cs
@@ -29,5 +29,18 @@ namespace DynamicFilters.Tests
 
             Assert.Equal(result1, result2);
         }
+
+        [Theory]
+        [ClassData(typeof(MultipleOptionFiltartionBehaviorDataLoader))]
+        public void FiltrationBehavior_MultipleOption_Success<T>(
+            List<T> subjectToFiltering,
+            DynamicFilterBase<T> filter,
+            Func<T, bool> analogousPredicate)
+        {
+            IEnumerable<T> result1 = subjectToFiltering.Where(filter);
+            IEnumerable<T> result2 = subjectToFiltering.Where(analogousPredicate);
+
+            Assert.Equal(result1, result2);
+        }
     }
 }
