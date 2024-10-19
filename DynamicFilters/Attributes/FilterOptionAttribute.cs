d7b68a38e587c09bc8f741599a391cf45063b408
Ask-DNA <alexander.sukhanov.11@gmail.com>
2024-10-19 16:27:11 +0300

    feat(FilterOptionAttribute)!: remove constructors
    For ease of use, attribute configuration is now done through direct access to its properties

--- DynamicFilters/Attributes/FilterOptionAttribute.cs
+++ DynamicFilters/Attributes/FilterOptionAttribute.cs
@@ -3,28 +3,10 @@
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
     public class FilterOptionAttribute : Attribute
     {
-        internal FilterOptionType Option { get; init; }
+        public FilterOptionType Option { get; init; } = FilterOptionType.Equality;
 
-        internal string? TargetName { get; init; } = null;
+        public string? TargetName { get; init; } = null;
 
-        internal string? IgnoreFlagName { get; init; } = null;
-
-        public FilterOptionAttribute(FilterOptionType option, string targetName, string ignoreFlagName)
-        {
-            Option = option;
-            TargetName = targetName;
-            IgnoreFlagName = ignoreFlagName;
-        }
-
-        public FilterOptionAttribute(FilterOptionType option, string targetName)
-        {
-            Option = option;
-            TargetName = targetName;
-        }
-
-        public FilterOptionAttribute(FilterOptionType option)
-        {
-            Option = option;
-        }
+        public string? IgnoreFlagName { get; init; } = null;
     }
 }
